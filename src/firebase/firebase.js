7db6a385413891f6394dfacc8311eddf9dadb3de
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-19 11:39:20 +0200

    Fix errors in test database setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyA20FN_3_FFK4vPaL7yi9ehUfnFxChgLXE",
-  authDomain: "expensify-e2fb7.firebaseapp.com",
-  databaseURL: "https://expensify-e2fb7.firebaseio.com",
-  projectId: "expensify-e2fb7",
-  storageBucket: "expensify-e2fb7.appspot.com",
-  messagingSenderId: "865004766510"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
